364f7cc3bd3e5fe28d5de19e64bbbf0da88bccad
ojh102 <ojh102@gmail.com>
2017-03-22 18:44:17 +0900

    lottie 적용

--- app/src/main/kotlin/com/github/ojh/overtime/splash/SplashActivity.kt
+++ app/src/main/kotlin/com/github/ojh/overtime/splash/SplashActivity.kt
@@ -2,16 +2,24 @@ package com.github.ojh.overtime.splash
 
 import android.content.Intent
 import android.os.Bundle
+import com.airbnb.lottie.LottieAnimationView
 import com.github.ojh.overtime.R
 import com.github.ojh.overtime.base.BaseActivity
 import com.github.ojh.overtime.di.AppComponent
 import com.github.ojh.overtime.timeline.TimeLineActivity
+import kotlinx.android.synthetic.main.activity_splash.*
 import javax.inject.Inject
 
 class SplashActivity : BaseActivity(), SplashContract.View {
 
     @Inject
     lateinit var presenter: SplashPresenter<SplashContract.View>
+
+    val lottieViews by lazy(LazyThreadSafetyMode.NONE) {
+        arrayOf<LottieAnimationView>(
+                lottie_o, lottie_v, lottie_e1, lottie_r, lottie_t, lottie_i, lottie_m, lottie_e2
+        )
+    }
 
     override fun setComponent(appComponent: AppComponent) {
         DaggerSplashComponent.builder()
@@ -24,7 +32,7 @@ class SplashActivity : BaseActivity(), SplashContract.View {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         presenter.attachView(this)
-        presenter.init()
+        presenter.init(lottieViews)
     }
 
     override fun navigateToTimeLine() {
--- app/src/main/kotlin/com/github/ojh/overtime/splash/SplashPresenter.kt
+++ app/src/main/kotlin/com/github/ojh/overtime/splash/SplashPresenter.kt
@@ -1,9 +1,13 @@
 package com.github.ojh.overtime.splash
 
+import android.os.Handler
+import com.airbnb.lottie.LottieAnimationView
 import com.github.ojh.overtime.base.BasePresenter
 import com.github.ojh.overtime.data.DataManager
 import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
@@ -13,11 +17,21 @@ class SplashPresenter<V : SplashContract.View> @Inject constructor(
 
 ) : BasePresenter<V>(dataManager, compositeDisposable), SplashContract.Presenter<V> {
 
-    override fun init() {
+    override fun init(views: Array<LottieAnimationView>) {
+
         compositeDisposable.add(
-                Observable.timer(2, TimeUnit.SECONDS).subscribe {
-                    getView()?.navigateToTimeLine()
-                }
+                Observable.intervalRange(0, views.size.toLong(), 0, 300, TimeUnit.MILLISECONDS)
+                        .map(Long::toInt)
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .doOnComplete {
+                            Handler().postDelayed({
+                                getView()?.navigateToTimeLine()
+                            }, 1000)
+                        }
+                        .subscribe {
+                            views[it].playAnimation()
+                        }
         )
     }
 
